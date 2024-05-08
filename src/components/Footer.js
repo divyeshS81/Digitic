e7fafbad590c146710705199b3b66c4e92266180
divyeshS81 <divyeshsarvaiya999@gmail.com>
2024-05-08 11:38:56 +0530

    complate a header in this commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <>
+      <footer></footer>
+      <footer></footer>
+      <footer></footer>
+    </>
   )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -98,33 +98,33 @@ const Header = () => {
                 <div>
                   <div className="dropdown">
                     <button
-                      className="btn btn-secondary dropdown-toggle bg-transparent border-0 gap-15 d-flex align-items-center"
+                      className="btn btn-secondary dropdown-toggle bg-transparent border-0  gap-15 d-flex align-items-center"
                       type="button"
                       id="dropdownMenuButton1"
                       data-bs-toggle="dropdown"
                       aria-expanded="false"
                     >
                       <img src="images/menu.svg" alt="" />
-                      <span>Shop Categories</span>
+                      <span className="me-5 d-inline-block">Shop Categories</span>
                     </button>
                     <ul
                       className="dropdown-menu"
                       aria-labelledby="dropdownMenuButton1"
                     >
                       <li>
-                        <a className="dropdown-item" href="#">
+                        <Link className="dropdown-item text-white" to="">
                           Action
-                        </a>
+                        </Link>
                       </li>
-                      <li>
-                        <a className="dropdown-item" href="#">
+                      <li> 
+                        <Link className="dropdown-item text-white" to="">
                           Another action
-                        </a>
+                        </Link>
                       </li>
                       <li>
-                        <a className="dropdown-item" href="#">
+                        <Link className="dropdown-item text-white" to="">
                           Something else here
-                        </a>
+                        </Link>
                       </li>
                     </ul>
                   </div>
